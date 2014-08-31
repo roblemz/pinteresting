b6c70f08217a14a909dcf5cd48f92eeaae908502
Rob <rob.flessner@gmail.com>
2014-08-31 13:41:38 -0500

    added users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Note Set this to the actual host name
+  config.action_mailer.default_url_options = { host: 'stormy-peak-6469.herokuapp.com', port: 3000 }
 end
