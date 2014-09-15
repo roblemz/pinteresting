4f4f6390a04aa9a69fc27d5f09d30aa09655ad06
Rob <rob.flessner@gmail.com>
2014-09-15 08:30:22 -0500

    Add Amazon s3 for images

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '~> 1.20.0'
 
 group :development, :test do
      gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ Rails.application.configure do
 
   #Note Set this to the actual host name
   config.action_mailer.default_url_options = { host: 'stormy-peak-6469.herokuapp.com', port: 3000 }
+
+  #Sets app to use Amazing S3 for images
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+   }
+  }
+
 end
